6ecbff851cf4067dc4ba40ba38b296c28fac8408
Sourfruit <hello@def-not-hacking-the.net>
2022-08-21 22:57:53 -0400

    Takeout.py - first commit

---
+++ README.md
@@ -0,0 +1,82 @@
+<p align='center'><img src="https://i.ibb.co/s9kq3V0/takeout.png" height="150px"/></p>
+<h1 align='center'>Takeout.py</h1>
+
+<p align='center'>
+    <img display="inline-block" src="https://img.shields.io/pypi/v/takeout.py?style=for-the-badge" /> <img display="inline-block" src="https://img.shields.io/badge/Made%20with-Python-green?style=for-the-badge" />
+</p>
+<p align='center'>Takeout.py is super easy to use. In under 10 lines of code, you can send an email to anyone, anywhere. Ah, the joys of the internet.</p>
+
+## Installation 🏗
+You can install Takeout.js using pip
+```shell
+$ pip install takeout.py
+```
+
+Then, import it like this:
+```py
+from takeout import TakeoutClient
+```
+
+## Setup 🛠
+First, get your token from the [Takeout dashboard](https://takeout.bysourfruit.com/dashboard). You'll need it in a little bit.
+
+Then, using that token you just got, use it here: 
+```py
+TakeoutClient.login('your token here')
+```
+
+As of right now, your code should look something like this:
+```py
+from takeout import TakeoutClient
+TakeoutClient.login('your token here')
+```
+
+"But I want to actually send an email!" => we're getting there!  
+
+## Sending your first email 📤
+
+Define a 'template' similar to this: 
+```py
+emailTemplate = {
+    'to': 'test@example.com',
+    'from': 'Takeout.py', # as of July 2022, this will be (e.g) 'Takeout.js via Takeout' for free users
+    'subject': 'I just sent an email using Takeout!',
+    'html': "<b>My first email!</b>",
+}
+```
+or
+```py
+emailTemplate = {
+    'to': 'test@example.com',
+    'from': 'Takeout.py', # as of July 2022, this will be (e.g) 'Takeout.py via Takeout' for free users
+    'subject': 'I just sent an email using Takeout!',
+    'text': 'My first email!',
+}
+```
+and then... 
+```py
+TakeoutClient.send(**emailTemplate)
+```
+Note! Unlike Takeout.js, Takeout.py does not yet return an email ID.
+
+See? It's super simple. Oh, and you can also import HTML directly from a file, using getHTMLFileContents(). 
+This is demonstrated here: 
+```py
+file = TakeoutClient.getHTMLFileContents('test.html')
+emailTemplate = {
+    'to': 'test@example.com',
+    'from': 'Takeout.py', # as of July 2022, this will be (e.g) 'Takeout.js via Takeout' for free users
+    'subject': 'I just sent an email using Takeout!',
+    'html': file,
+}
+
+TakeoutClient.send(**emailTemplate)
+```
+
+## Roadmap 🚦
+- Some sort of templating built in, allowing you a greater variety of options in a single package (with... well some dependencies). 
+- Fixing SMTP email validation.
+- Bug fixes.
+- A lot more.
+
+### See complete examples in [examples/](https://github.com/s0urfruit/takeout.py/tree/main/examples)
---
+++ takeout/takeout.py
@@ -0,0 +1,91 @@
+import requests
+
+authenticated = None
+auth_token = None
+
+send_url = "https://takeout.bysourfruit.com/api/email/send"
+auth_url = "https://takeout.bysourfruit.com/api/auth/verify"
+
+
+class TakeoutClient:
+    @staticmethod
+    def login(token):
+        global authenticated
+        global auth_token
+
+        if token is None:
+            exit("Takeout Login Error! A token wasn't provided")
+
+        loginAttempt = requests.post(auth_url, json = {"token": token}, headers= {"Content-Type": "application/json"}, verify=True)
+        loginJSON = loginAttempt.json()
+
+        if loginAttempt.status_code > 200:
+            authenticated = False
+            exit(f'Takeout Login Error! {loginJSON["message"]}')
+
+        if loginAttempt.status_code == 200:
+            authenticated = True
+            auth_token = token
+            print("Authenticated successfully.")
+
+        if loginAttempt.status_code == 500:
+            exit("Takeout's currently having some issues server-side. These may be temporary issues caused by maintenance, or they may be caused by your input/Takeout.py.")
+
+
+    @staticmethod
+    def send(**kwargs):
+        if not authenticated:
+            exit("Takeout Send Error! You're not logged in! Run TakeoutClient.login(token).")
+
+        sender = kwargs.get("from").strip()
+        receiver = kwargs.get("to").strip()
+        subject = kwargs.get("subject").strip()
+        bodyText = kwargs.get("text", "")
+        bodyHTML = kwargs.get("html", "")
+
+        if not sender or not receiver or not subject:
+            exit("Takeout Send Error! One of the required fields to send an email was not fulfilled. Check if your receiver, sender, and subject are defined.")
+
+        else:
+            sendData = {
+                "token": auth_token,
+                "sender": sender,
+                "receiver": receiver,
+                "subject": subject,
+                # No bodies, yet !
+            }
+
+            if bodyText:
+                sendData.update({"bodyText": bodyText})
+
+            if bodyHTML:
+                sendData.update({"bodyHTML": bodyHTML})
+
+            if bodyHTML and bodyText:
+                sendData.update({"bodyHTML": bodyHTML})
+
+            # if not bodyText and not bodyHTML:
+                # print("You've supplied no bodies, but Takeout will send it along anyways...")
+
+            sendAttempt = requests.post(send_url, json=sendData, headers={"Content-Type": "application/json"}, verify=True)
+            sendJSON = sendAttempt.json()
+
+            if sendAttempt.status_code == 200:
+                print("Sent email successfully.")
+
+            if sendAttempt.status_code == 500:
+                exit("Takeout's currently having some issues server-side. These may be temporary issues caused by maintenance, or they may be caused by your input/Takeout.py.")
+
+            if sendAttempt.status_code > 200:
+                exit(f'Takeout Send Error! {sendJSON["message-id"]}')
+
+
+    @staticmethod
+    def getHTMLFileContents(path):
+        try:
+            file = open(path, mode='r')
+            all_of_it = file.read()
+            file.close()
+            return all_of_it
+        except IOError:
+            exit("Takeout File Error! File doesn't seem to exist")
